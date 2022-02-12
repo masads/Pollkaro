e11eeb78dd48c22022956b5f4567e83a26daa084
masads <asadsummair2019@gmail.com>
2022-02-13 01:30:58 +0500

    image issue

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,11 @@
+const conn = require("../config/database.js");
+
 const signup = async (req, res, next) => {
+    console.log(req.body);
+    res.status(200).json({ message: "register" });
+};
+const login = async (req, res, next) => {
     
 };
 
-module.exports = { signup };
+module.exports = { signup,login };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "./images");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage,
+});
 const { signup, login} = require("../controllers/auth.js");
+const { saveImage} = require("../controllers/images.js");
 
 
 const router = express.Router();
 
 router.post("/login", login);
-
-router.post("/signup", signup);
+router.post("/signup" , signup);
+router.post("/images", upload.array('image',900), saveImage);
 
 module.exports = router;
